05feefe7fa08da45c57f02d50bc06f46b1d32a46
mariaro833 <masha.rozhnova@gmail.com>
2022-01-12 19:19:54 +0200

    instructions for solve function

--- fillit/solve.c
+++ fillit/solve.c
@@ -6,15 +6,26 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 17:49:21 by thakala           #+#    #+#             */
-/*   Updated: 2022/01/11 18:01:59 by thakala          ###   ########.fr       */
+/*   Updated: 2022/01/12 14:54:42 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-	
+	Receives as input the bitarray list of tetrimino shorts
+		and smallest possible board_size calculated.
+	Solves the smallest possible board.
+	Returns the board for displaying in alphabets.
 */
 
-char	solve()
+/*
+	The board state is stored in bits.
+	Bits can only tell if there is a square of a piece in the position,
+		and nothing else (no letter).
+	Unless constructing the alphabetical board solution can be done,
+		the solution is not a solution.
+*/
+
+char	*solve(long **bitarray, short *tetriminos, unsigned char board_size)
 {
 	
 }
