b32131b68a9581dfbf5120e9347e43969e21ce0e
Teemu Hakala <thakala@c1r6p16.hive.fi>
2022-01-16 17:09:50 +0200

    pad_short before debugging

--- fillit/fillit.h
+++ fillit/fillit.h
@@ -6,11 +6,14 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 16:51:32 by thakala           #+#    #+#             */
-/*   Updated: 2022/01/11 16:51:51 by thakala          ###   ########.fr       */
+/*   Updated: 2022/01/16 12:24:59 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
+
+unsigned long	pad_short(short tetrimino, unsigned long index, \
+	unsigned char board_width);
 
 #endif
--- fillit/pad_short.c
+++ fillit/pad_short.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 10:51:26 by thakala           #+#    #+#             */
-/*   Updated: 2022/01/13 10:52:09 by thakala          ###   ########.fr       */
+/*   Updated: 2022/01/16 16:31:18 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ unsigned long	pad_short(short tetrimino, unsigned long index, \
 	unsigned char	padding;
 
 	t = 16;
-	if (board_width > 4)
+	if (board_width > 4 && board_width <= 20)
 	{
 		padding = board_width - 4;
 		while (t)
@@ -29,8 +29,10 @@ unsigned long	pad_short(short tetrimino, unsigned long index, \
 			tetrilong = (tetrilong << (index + 16 - t)) ^ tetrimino_line;
 			tetrilong = tetrilong << padding;
 		}
-		return ();
+		return (tetrilong);
 	}
+	else if (board_width == 4)
+		return (((unsigned long)tetrimino) << 48);
 	else
-		return (debug_error());
+		return ((unsigned long)(-1));
 }
