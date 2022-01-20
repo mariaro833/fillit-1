261b6d26461bddcef2dd9ac72e183b4223a8c927
mariaro833 <masha.rozhnova@gmail.com>
2022-01-20 12:15:08 +0200

    200122

--- fillit/reader.c
+++ fillit/reader.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_tetrimino.c                               :+:      :+:    :+:   */
+/*   reader.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mrozhnova <mrozhnova@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/11 15:47:39 by thakala           #+#    #+#             */
-/*   Updated: 2022/01/20 11:27:46 by mrozhnova        ###   ########.fr       */
+/*   Created: 2022/01/20 12:13:48 by mrozhnova         #+#    #+#             */
+/*   Updated: 2022/01/20 12:14:24 by mrozhnova        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,4 +49,51 @@ ssize_t	get_next_tetrimino(int fd, int *status, char ***tetrimino)
 		line++;
 	}
 	return ();
+}
+
+# define TETRIMINO_WIDTH 4 //move to .h file
+
+
+
+static unsigned char	get_index(line_idx, character_idx)
+{
+	return ();
+}
+
+/*
+	Assumes valid length of lines.
+	# (hashtag) for tetrimino
+*/
+
+unsigned short	convert_to_short(const char **tetrimino_string)
+{
+	unsigned short	tetrimino;
+	unsigned char	line_idx;
+	unsigned char	character_idx;
+	char			character;
+
+	tetrimino = 0;
+	line_idx = 0;
+	while (line_idx < TETRIMINO_WIDTH)
+	{
+		//if (ft_strlen(tetrimino_string[line_idx]) != 4)
+		character_idx = TETRIMINO_WIDTH;
+		while (0 <= character_idx + 1)
+		{
+			if (tetrimino_string[line_idx][character_idx] == '#')
+			{
+				tetrimino = tetrimino | (0x1 << get_index(line_idx, character_idx));
+			}
+			else if ()
+			{
+				tetrimino = 0b0110110000000000;
+			}
+			else
+			{
+
+			}
+			character_idx--;
+		}
+		line_idx--;;
+	}
 }
