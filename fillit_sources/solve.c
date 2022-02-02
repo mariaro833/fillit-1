5d689c1d8f3145f61d27ac1fc114b8b6d1ca1e92
teemu-hakala <78655964+teemu-hakala@users.noreply.github.com>
2022-02-02 06:03:29 +0200

    skip_index conditional return since I_1 pieces would overrun the board? calculating index first in set_index

--- fillit_sources/solve.c
+++ fillit_sources/solve.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 18:31:19 by mrozhnova         #+#    #+#             */
-/*   Updated: 2022/02/02 04:24:57 by thakala          ###   ########.fr       */
+/*   Updated: 2022/02/02 05:22:06 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ uint16_t	set_index(t_tetri *tetriminoes, uint8_t board_size, \
 	uint16_t	index;
 
 	previous = *(tetriminoes - tetriminoes->previous);
+	index = board_size * previous.row + previous.col + \
+		previous.packing_gap * (!!tetriminoes->previous);
 	tetriminoes->row = previous.row;
 	sum = previous.col + previous.packing_gap;
 	if (sum > board_size - tetriminoes->width)
@@ -44,10 +46,10 @@ uint16_t	set_index(t_tetri *tetriminoes, uint8_t board_size, \
 		tetriminoes->row++;
 		tetriminoes->col = sum % board_size;
 	}
+	else
+		tetriminoes->col = sum;
 	if (tetriminoes->row > board_size - tetriminoes->height)
 		return ((uint16_t)(-1));
-	index = board_size * previous.row + previous.col + \
-		previous.packing_gap * (!!tetriminoes->previous);
 	while (bitarrcheck(bitarr, index))
 		index++;
 	return (index);
@@ -60,9 +62,8 @@ uint8_t	skip_index(uint16_t *index, t_tetri *tetrimino, uint16_t board_size)
 		tetrimino->col = 0;
 		tetrimino->row++;
 		*index += tetrimino->width - 1;
-		return (tetrimino->row <= board_size - tetrimino->height);
 	}
-	return (1);
+	return (tetrimino->row <= board_size - tetrimino->height);
 }
 
 char	*solve(t_tetri *tetriminoes, uint16_t board_size, t_bitarr *bitarr, \
